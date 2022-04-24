ef790193fb4b547000cbcb3d8f4d90e38a4c97c0
votth <voth998@gmail.com>
2022-04-25 01:47:32 +0200

    Redirectorized: Cpp/ and Python/

--- scripting/grep_awk.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-echo $(ip a | grep /24 | awk '{print $2}')
--- scripting/read_name.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo "Who are you?"
-read name
-if [ $name ]; then
-	echo $name is you.
-else
-	echo Name field is empty.
-fi
